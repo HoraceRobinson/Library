c6cc6e4115fd569efb58edc55bf56369d5ac949e
HoranceRoinson <3386875092@qq.com>
2022-05-26 14:12:14 +0800

    add basic AddBookActivity

--- app/src/main/java/com/example/library/AddBookActivity.java
+++ app/src/main/java/com/example/library/AddBookActivity.java
@@ -3,12 +3,24 @@ package com.example.library;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 
 public class AddBookActivity extends AppCompatActivity {
+    private EditText isbn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_book);
     }
+
+    public void btn3Click(View view){
+        isbn = findViewById(R.id.isbn);
+        LinearLayout li = findViewById(R.id.linear1);
+        li.setVisibility(View.VISIBLE);
+        String text = isbn.getText().toString();
+    }
+
 }
--- app/src/main/java/com/example/library/MainActivity.java
+++ app/src/main/java/com/example/library/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.library;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.service.autofill.TextValueSanitizer;
 import android.view.View;
@@ -14,17 +15,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-//        Button btn1 = findViewById(R.id.btn1);
-//        btn1.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//
-//            }
-//        });
-
     }
 
     public void btn1Click(View view) {
+        startActivity(new Intent(this, AddBookActivity.class));
+    }
+
+    public void btn2Click(View view){
+        startActivity(new Intent(this, BookListActivity.class));
     }
 }

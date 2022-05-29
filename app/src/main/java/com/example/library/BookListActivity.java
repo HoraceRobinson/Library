bee6c6cc2b9038e510021c062218910dc3ac8dce
HoranceRoinson <3386875092@qq.com>
2022-05-29 18:24:06 +0800

    add sort function

--- app/src/main/java/com/example/library/BookListActivity.java
+++ app/src/main/java/com/example/library/BookListActivity.java
@@ -10,19 +10,28 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 public class BookListActivity extends AppCompatActivity {
 
     public static JSONArray bookList = new JSONArray();
     public static JSONArray result = new JSONArray();
+    public static List<String> list = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +50,15 @@ public class BookListActivity extends AppCompatActivity {
         String text = "共" + bookList.length() + "条记录";
         numHint.setText(text);
 
+        list = new ArrayList<>();
+        list.add("按作者名排序");
+        list.add("按出版日期排序");
+
+        Spinner spinner = findViewById(R.id.spinner);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, list);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
         Button queryBtn = findViewById(R.id.btn5);
         queryBtn.setOnClickListener(view -> {
             EditText editText = findViewById(R.id.search_text);
@@ -53,6 +71,60 @@ public class BookListActivity extends AppCompatActivity {
                 }
             }).start();
         });
+
+        spinner.setOnItemSelectedListener(new Spinner.OnItemSelectedListener() {
+            public void onItemSelected(AdapterView<?> argO, View argl, int arg2, long arg3) {
+                argO.setVisibility(View.VISIBLE);
+                String key = adapter.getItem(arg2);
+                try {
+                    sortList(key);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+            public void onNothingSelected(AdapterView<?> argO) {
+                argO.setVisibility(View.VISIBLE);
+            }
+        });
+    }
+
+    private void sortList(String key) throws JSONException {
+        JSONArray sortedJsonArray = new JSONArray();
+
+        List<JSONObject> jsonValues = new ArrayList<JSONObject>();
+        for (int i = 0; i < result.length(); i++) {
+            jsonValues.add(result.getJSONObject(i));
+        }
+        jsonValues.sort(new Comparator<JSONObject>() {
+            private String KEY_NAME = null;
+
+            @Override
+            public int compare(JSONObject a, JSONObject b) {
+                String valA = new String();
+                String valB = new String();
+                if(key.equals("按作者名排序")){
+                    KEY_NAME = "author";
+                }
+                else if(key.equals("按出版日期排序")){
+                    KEY_NAME = "date";
+                }
+                try {
+                    valA = (String) a.get(KEY_NAME);
+                    valB = (String) b.get(KEY_NAME);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+                return valA.compareTo(valB);
+            }
+        });
+
+        for (int i = 0; i < result.length(); i++) {
+            sortedJsonArray.put(jsonValues.get(i));
+        }
+        result = sortedJsonArray;
+        System.out.println(result);
+        handler.sendEmptyMessage(0x00);
     }
 
     private void getAuthorList(String author_name) throws JSONException {
